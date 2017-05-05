b1086464c16d8bfc6066b4403f1743d53f6cfa64
David Grimbichler <david@grimbichler.ch>
2017-05-05 16:13:22 +0200

    Project Init
    - Control app with select button(start and stop)
    - First try calculations with the x-axis

--- src/c/main.c
+++ src/c/main.c
@@ -15,13 +15,17 @@
 #include <pebble_fonts.h>
   
 #define TAP_NOT_DATA false
-  
+
 /********************************************************************
 * Variables
 *
 ********************************************************************/
 static Window *s_main_window;
 static TextLayer *s_output_layer;
+
+static GRect window_frame;
+static bool isRunning;
+static bool getStartValue;
 
 /********************************************************************
 * Routines
@@ -36,20 +40,30 @@ static TextLayer *s_output_layer;
 static void data_handler(AccelData *data, uint32_t num_samples) {
   // Long lived buffer
   static char s_buffer[128];
-  static int32_t num_average[3];
-
+  //tatic int32_t num_average[1];
+  static int32_t dist;
+  static int32_t x_zero;
+  
+  if(getStartValue)
+    {
+    x_zero = ((int32_t)(data[0].x)+(int32_t)(data[1].x)+(int32_t)(data[2].x)+(int32_t)(data[3].x)+(int32_t)(data[4].x))/5;
+    getStartValue = false;
+    dist = 0;
+  }
   // Calculate average
-  num_average[0]=((int32_t)(data[0].x)+(int32_t)(data[1].x)+(int32_t)(data[2].x))/3;
-  num_average[1]=((int32_t)(data[0].y)+(int32_t)(data[1].y)+(int32_t)(data[2].y))/3;  
-  num_average[2]=((int32_t)(data[0].z)+(int32_t)(data[1].z)+(int32_t)(data[2].z))/3;
+  //num_average[0]=((int32_t)(data[0].x)+(int32_t)(data[1].x)+(int32_t)(data[2].x))/3;
+  //num_average[1]=((int32_t)(data[0].y)+(int32_t)(data[1].y)+(int32_t)(data[2].y))/3;  
+  //num_average[2]=((int32_t)(data[0].z)+(int32_t)(data[1].z)+(int32_t)(data[2].z))/3;
+  for(uint32_t i=0;i<num_samples;i++)
+    {
+    dist += ((x_zero - (int32_t)(data[i].x))*9.810)/25/1000;
+  }
   
   // Compose string of all data
   snprintf(s_buffer, sizeof(s_buffer), 
-    "N X,Y,Z\n0 %d,%d,%d\n1 %d,%d,%d\n2 %d,%d,%d\nA %d %d %d", 
-    data[0].x, data[0].y, data[0].z, 
-    data[1].x, data[1].y, data[1].z, 
-    data[2].x, data[2].y, data[2].z,
-    (int)num_average[0], (int)num_average[1], (int)num_average[2]       
+    "App is running\n\nZero: %d\nDist: %d mm",
+    (int)x_zero,
+    (int)dist
   );
 
   //Show the data
@@ -88,6 +102,48 @@ static void tap_handler(AccelAxisType axis, int32_t direction) {
   }
 }
 
+
+/*===================================================================
+* Callback-Handler
+===================================================================*/
+void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+  //... called on single click, and every 1000ms of being held ...
+  //Window *window = (Window *)context;
+  static char s_buffer[20];
+  if(isRunning)
+    {
+    isRunning = false;
+    accel_data_service_unsubscribe();
+    // Compose string of all data
+    snprintf(s_buffer, sizeof(s_buffer), 
+      "App is stopped"
+    );
+
+  //Show the data
+  text_layer_set_text(s_output_layer, s_buffer);
+  }
+  else
+    {
+    isRunning = true;
+    getStartValue = true;
+    // Subscribe to the accelerometer data service
+    int num_samples = 5;
+    accel_data_service_subscribe(num_samples, data_handler);
+
+    // Choose update rate
+    accel_service_set_sampling_rate(ACCEL_SAMPLING_25HZ);
+  }
+}
+
+/*===================================================================
+* Callback-Handler
+===================================================================*/
+void config_provider(Window *window) {
+ // single click / repeat-on-hold config:
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_single_click_handler);
+
+}
+
 /********************************************************************
 * Init-code
 *
@@ -98,12 +154,12 @@ static void tap_handler(AccelAxisType axis, int32_t direction) {
 ===================================================================*/
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-  GRect window_bounds = layer_get_bounds(window_layer);
+  GRect frame = window_frame = layer_get_frame(window_layer);
 
   // Create output TextLayer
-  s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 10, window_bounds.size.h));
+  s_output_layer = text_layer_create(GRect(5, 0, frame.size.w - 10, frame.size.h));
   text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text(s_output_layer, "No data yet.");
+  text_layer_set_text(s_output_layer, "Press select.");
   text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
 }
@@ -119,7 +175,7 @@ static void main_window_unload(Window *window) {
 /*===================================================================
 * initialising Watchface
 ===================================================================*/
-static void init() {
+static void init(void) {
   // Create main Window
   s_main_window = window_create();
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -128,24 +184,22 @@ static void init() {
   });
   window_stack_push(s_main_window, true);
 
+  window_set_click_config_provider(s_main_window, (ClickConfigProvider) config_provider);
+  
+  isRunning = false;
+  getStartValue = false;
+  
   // Use tap service? If not, use data service
   if (TAP_NOT_DATA) {
     // Subscribe to the accelerometer tap service
     accel_tap_service_subscribe(tap_handler);
-  } else {
-    // Subscribe to the accelerometer data service
-    int num_samples = 3;
-    accel_data_service_subscribe(num_samples, data_handler);
-
-    // Choose update rate
-    accel_service_set_sampling_rate(ACCEL_SAMPLING_25HZ);
-  }
+  } 
 }
 
 /*===================================================================
 * deinitialising Watchface
 ===================================================================*/
-static void deinit() {
+static void deinit(void) {
   // Destroy main Window
   window_destroy(s_main_window);
 
